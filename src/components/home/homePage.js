f8a96c7445a8bc37f1a772c491644dc1e45d2501
gkhollingshead <gary.k.hollingshead@gmail.com>
2017-03-07 21:24:09 -0800

    inital commit

---
+++ src/components/home/homePage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Link} from "react-router";
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Home Page</h1>
+        <Link to="about" className="btn btn-primary btn-group-lg">
+          About
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
---
+++ tools/startMessage.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-console */
+
+console.log("Starting Radmin in dev mode...");
